96e66443959d0ea454acdd350fbbf37b365577ac
Younghu Park <younghu@Willow.local>
2019-05-30 00:36:20 -0700

    set up to get trending articles from NYTimes

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require('express')()
 const app = express
-const bodyParser = require('body-parser')()
 const request = require('request')
 const config = require('config')
 
 const apiKey= config.get('apiKey');
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  var url = 'https://newsapi.org/v2/top-headlines?' +
+           'sources=the-new-york-times&' +
+          'apiKey=' + apiKey;
+request(url, function (err, response, body) {
+    if(err){
+      console.log('stuff')
+    } else {
+      if(!response){
+        console.log('none');
+      } else {
+        var results = JSON.parse(body);
+        var articles = results.articles
+        var source = articles[1].source.name
+        res.render('index', {source: source, response: articles});
+      }
+    }
+  });
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
